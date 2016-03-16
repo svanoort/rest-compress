03bd93dfec81e95a7e1da644bb61f40a64de0695
Sam Van Oort <samvanoort@gmail.com>
2016-03-16 07:22:05 -0400

    Tiny-memory version of app

--- demo-app/run-docker.sh
+++ demo-app/run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t jboss-demo-app:0.7 . 
-docker run --rm -i -t -p 8080:8080 jboss-demo-app:0.7
+docker build -t jboss-demo-app:0.7-tinymem . 
+docker run --rm -i -t -p 8080:8080 jboss-demo-app:0.7-tinymem
